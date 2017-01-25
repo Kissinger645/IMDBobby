05ab98fa7b8d849e7c82815e009678e5b2b58f00
Bobby <kissinger645@gmail.com>
2017-01-24 21:17:58 -0500

    All but hard queries

---
+++ NormalQuery.sql
@@ -0,0 +1,5 @@
+﻿SELECT person.Age FROM Person JOIN Review ON Person.Id=REVIEW.Person WHERE movie = 1;
+SELECT Occupation FROM Person WHERE Age<40;
+SELECT title FROM movie WHERE [IMDB Url] IS NULL;
+SELECT title FROM movie JOIN review ON review.movie=movie.id WHERE rating<2;
+SELECT * FROM person JOIN review ON person.id=review.person JOIN movie ON review.movie=movie.id 
