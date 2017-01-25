6af8dc604e98c33e35d0b4da64a97fa89d8e708c
Bobby <kissinger645@gmail.com>
2017-01-25 06:00:53 -0500

    Normal query only

--- NormalQuery.sql
+++ NormalQuery.sql
@@ -2,4 +2,3 @@
 SELECT Occupation FROM Person WHERE Age<40;
 SELECT title FROM movie WHERE [IMDB Url] IS NULL;
 SELECT title FROM movie JOIN review ON review.movie=movie.id WHERE rating<2;
-SELECT * FROM person JOIN review ON person.id=review.person JOIN movie ON review.movie=movie.id 
